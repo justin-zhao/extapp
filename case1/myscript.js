bf6614c219551eeaeb5009c0a03e477a0f91c12f
Justin Zhao <justin-zhao@qq.com>
2017-03-30 13:45:11 +0800

    Initilize extension app for Chrome
    Signed-off-by: Justin Zhao <justin-zhao@qq.com>

---
+++ case1/myscript.js
@@ -0,0 +1,8 @@
+
+console.log("my test");
+alert("My Test");
+
+//c=document.getElementById("myCanvas");
+//cxt=c.getContext("2d");
+
+//alert("width="+c.width);
---
+++ case2/myscript.js
@@ -0,0 +1,14 @@
+﻿let su=document.getElementById("su"); 
+su.value = "My Search"; 
+let kw=document.getElementById("kw"); 
+kw.value = "Chrome Extension App"; 
+su.onclick= function() { 
+	let today = new Date(); 
+	alert("My search\n" + "date:" + today.toString()); 
+}; 
+function myInterval() { 
+	su.click(); 
+	//alert("Hello, Test");
+} 
+
+setInterval("myInterval()",5000);
